8595cbcb820a27161bace5db263619498fc905d8
sam_sad <lr24092003@yandex.ru>
2021-02-04 18:06:10 +0300

    third commit

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import requests
 
 params = {'ll': [37.530887, 55.703118], 'spn': [0.002, 0.002], 'l': 'map'}
 # Запишем полученное изображение в файл.
+spn_limits = [0.00001, 90]
+
+
 response = get_map(params)
 map_file = "map.png"
 with open(map_file, "wb") as file:
@@ -23,14 +26,17 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
-                params['spn'][0] += 0.002
-                params['spn'][1] += 0.002
+                params['spn'][0] /= 2
+                params['spn'][1] /= 2
+                params['spn'][0] = max(params['spn'][0], spn_limits[0])
+                params['spn'][1] = max(params['spn'][1], spn_limits[0])
             elif event.key == pygame.K_PAGEDOWN:
-                params['spn'][0] -= 0.002
-                params['spn'][1] -= 0.002
+                params['spn'][0] *= 2
+                params['spn'][1] *= 2
+                params['spn'][0] = min(params['spn'][0], spn_limits[1])
+                params['spn'][1] = min(params['spn'][1], spn_limits[1])
             with open(map_file, 'wb') as file:
                 file.write(get_map(params))
-
 
     screen.blit(pygame.image.load(map_file), (0, 0))
     # Переключаем экран и ждем закрытия окна.
